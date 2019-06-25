ac5c42eeee396ba4d5d1f92ea1c8928da143cdbf
Geir Sørensen <geir.sorensen@dnvgl.com>
2019-06-25 12:53:03 +0200

    First working version

---
+++ src/test1.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+interface IProps {
+    name: string;
+}
+export const NameField: React.FC<IProps> = (props) => {
+    const { children, name } = props;
+    return <span>{props.name}</span>;
+};
+
+// export const NameField2: React.FC = () => {
+//     return "Kalle";
+// };
+
+export const NameBlock: React.FC<{name: string}> = (props) => {
+    return <NameField name={props.name}></NameField>
+};
